03f9d64a9cf7b04fa19520cc224514fa024e8824
tomaspakala <tomaspakala@gmail.com>
2016-11-30 23:28:29 +0200

    Adding missing files

---
+++ SimpleTestsForSimpleTaylor/TaylorApproxerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using SimpleApproxTaylorSeries;
+
+namespace SimpleTestForSimpleTaylor
+{
+    [TestFixture]
+    public class TaylorApproxerTests
+    {
+        [TestCase (3, 0.5, 4)]
+        [TestCase (3, 0.5, 8)]
+        public void Power(double x, double power, int accuracy)
+        {
+            double accuracity = Math.Pow(10, accuracy + 1);
+
+            var res = TaylorApproxer.Power(x, power, accuracy);
+            
+            Equals(Math.Truncate(accuracity * x) / accuracity, res);
+        }
+    }
+}
