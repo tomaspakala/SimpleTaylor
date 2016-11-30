bd27d3ec3013690ccdf1370f0095efdf64c59b06
tomaspakala <tomaspakala@gmail.com>
2016-11-30 23:16:04 +0200

    Adding base functionality

---
+++ SimpleApproxTaylorSeries/TaylorApproxer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Numerics;
+
+namespace SimpleApproxTaylorSeries
+{
+    public static class TaylorApproxer
+    {
+        public static double Power(double x, double power, int accuracy)
+        {
+            Func<double, double> pow = delegate(double xx)
+            {
+                return Math.Pow(xx, power); 
+            };
+
+            double acc = Math.Pow(10, accuracy);
+            double adjusted = Adjuster(Math.Pow(x, power), acc);
+
+            int x0 = FindClose(x, power);
+
+            var sum = pow.Invoke(x0);
+
+            int i = 0;
+            while (adjusted != Adjuster(sum, acc))
+            {
+                ++i;
+                var f = MathNet.Numerics.Differentiate.DerivativeFunc(pow, i);
+
+                var r = (f.Invoke(x0)* Math.Pow(x - x0, i))/(double) MathNet.Numerics.SpecialFunctions.Factorial(new BigInteger(i));
+                sum += r;
+                Console.WriteLine(i + ". div - " + f.Invoke(x));
+                Console.WriteLine(i + ". R - " + r);
+                Console.WriteLine(i + ". T - " + sum);
+
+                if (i > 20)
+                    throw new Exception();
+            }
+
+            return sum;
+        }
+
+        private static double Adjuster(double x, double accuracity)
+        {
+            accuracity *= 10;
+            return Math.Truncate(accuracity * x) / accuracity;
+        }
+
+        private static int FindClose(double x, double power)
+        {
+            var close = (int)Math.Pow(x, power);
+
+            if (close == 1)
+                ++close;
+
+            while (Math.Pow(close, power) % 1 != 0)
+                close++;
+
+            return close;
+        }
+
+    }
+}
