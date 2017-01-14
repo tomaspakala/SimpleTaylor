f8c67e5705f07f1e1894c0434238d581b05180bf
Edvardas889 <etereikis@gmail.com>
2017-01-14 20:15:54 +0200

    Arctan

---
+++ SimpleApproxTaylorSeries/ArctangentApproxer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleApproxTaylorSeries
+{
+    public static partial class TaylorApproxer
+    {
+        public static double Arctangent(double x, int accuracy)
+        {
+            double sum = 0;
+            double lastSum = Double.MaxValue;
+            int i = 0;
+            while (Math.Abs(sum - lastSum) > 1 / Math.Pow(10, accuracy))
+            {
+                lastSum = sum;
+                double xRadian = Helper.DegreeToRadian(x);
+                sum += Math.Pow(-1, i) * Math.Pow(xRadian, 2 * i) / (2 * i);
+                ++i;
+            }
+            return sum;
+        }
+    }
+}
