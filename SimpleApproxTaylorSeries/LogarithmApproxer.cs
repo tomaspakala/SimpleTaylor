6ec3423ded06c536665e75a28f8a5daa99b8ae64
JustinasK <justinas94@gmail.com>
2017-01-14 18:54:30 +0200

    Logarithm fix

--- SimpleApproxTaylorSeries/LogarithmApproxer.cs
+++ SimpleApproxTaylorSeries/LogarithmApproxer.cs
@@ -12,40 +12,24 @@ namespace SimpleApproxTaylorSeries
 
             var sum = 0.0;
             int i = 0;
-
-            if (Math.Abs(x - 1.0) > 1)
-            {
-                sum = sum + LogarithmDiv(x - 1.0, accuracy);
-            }
+            var x1 = x - 1;
+            if (Math.Abs(x) > 1)
+                sum += Math.Log(x1);
 
             while (Math.Abs(sum - lastSum) > 1 / Math.Pow(10, accuracy))
             {
                 lastSum = sum;
                 ++i;
-                var r = Math.Pow(-1, i + 1) * Math.Pow(x - 1, i) / i;
-                sum += r;
-            }
-            return sum;
-        }
-
-        public static double LogarithmDiv(double x, int accuracy)
-        {
-            double lastSum = Double.MaxValue;
-
-            var sum = 0.0;
-            int i = 0;
-
-            if (Math.Abs(x - 1.0) > 1)
-            {
-                sum = sum + LogarithmDiv(x - 1.0, accuracy);
-            }
-
-            while (Math.Abs(sum - lastSum) > 1 / Math.Pow(10, accuracy))
-            {
-                lastSum = sum;
-                ++i;
-                var r = Math.Pow(-1, i + 1) / Math.Pow(x - 1, i) / i;
-                sum += r;
+                if (Math.Abs(x) <= 1)
+                {
+                    var r = Math.Pow(-1, i + 1) * Math.Pow(x1, i) / i;
+                    sum += r;
+                }
+                else
+                {
+                    var r = Math.Pow(-1, i + 1) * Math.Pow(x1, -1 * i) / i;
+                    sum += r;
+                }
             }
             return sum;
         }
