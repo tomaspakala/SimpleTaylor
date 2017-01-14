234399b601efcf100327fb29c40c06cc0a94ead6
tomaspakala <tomaspakala@gmail.com>
2017-01-14 21:42:02 +0200

    Simple UI improvements

--- SimpleTaylorSeriesUI/Calculator.cs
+++ SimpleTaylorSeriesUI/Calculator.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SimpleApproxTaylorSeries;
 
 namespace SimpleTaylorSeries
 {
@@ -15,6 +16,38 @@ namespace SimpleTaylorSeries
         public Calculator()
         {
             InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (listBox1.SelectedIndex)
+            {
+                case 0: // Cosh
+                case 1: // Sinh
+                    {
+                    textBox1.Enabled = true;
+                    textBox3.Enabled = true;
+                    button1.Enabled = true;
+                    break;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (listBox1.SelectedIndex)
+            {
+                case 0: // Cosh
+                    {
+                        label5.Text = TaylorApproxer.CosineHyper(Double.Parse(textBox1.Text), Int32.Parse(textBox3.Text)).ToString();
+                        break;
+                    }
+                case 1: // Sinh
+                    {
+                        label5.Text = TaylorApproxer.SineHyper(Double.Parse(textBox1.Text), Int32.Parse(textBox3.Text)).ToString();
+                        break;
+                    }
+            }
         }
     }
 }
